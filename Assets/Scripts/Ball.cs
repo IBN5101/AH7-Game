69ec5c056cb4e04ca8609abc3c1a1946ebe57643
IBN5101 <minhnghcm123@gmail.com>
2021-12-12 10:37:10 +0000

    Finished Hack
    2 hour run let's go.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public Vector2 movement;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("RandomMovement", 0.5f, 3);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(movement * Time.deltaTime * speed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("WallW"))
+            movement *= new Vector2(1, -1);
+        if (collision.gameObject.CompareTag("WallH"))
+            movement *= new Vector2(-1, 1);
+    }
+
+    public void RandomMovement()
+    {
+        movement = new Vector2(Random.Range(1, 5), Random.Range(1, 5));
+        speed = Random.Range(1, 3);
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
@@ -9,12 +7,18 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb;
     private float movementX, movementY;
 
+    [SerializeField] GameObject gameEnd;
+    [SerializeField] TextMeshProUGUI score;
+    public bool isGame = true;
+    public int scoreInt = 0;
+
     public float speed = 10;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        InvokeRepeating("IncreaseScore", 0.5f, 1);
     }
 
     void OnMove(InputValue movementValue)
@@ -24,10 +28,33 @@ public class PlayerController : MonoBehaviour
         movementY = movementVector.y;
     }
 
+    private void Update()
+    {
+
+        score.SetText("Score: " + scoreInt);
+    }
+
     void FixedUpdate()
     {
-        Vector2 movement = new Vector2(movementX, movementY);
-        //rb.AddForce(movement * speed);
-        transform.Translate(movement * Time.deltaTime * speed);
+        if (isGame)
+        {
+            Vector2 movement = new Vector2(movementX, movementY);
+            transform.Translate(movement * Time.deltaTime * speed);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ball"))
+        {
+            gameEnd.gameObject.SetActive(true);
+            isGame = false;
+            CancelInvoke("IncreaseScore");
+        }
+    }
+
+    public void IncreaseScore()
+    {
+        scoreInt++;
     }
 }
